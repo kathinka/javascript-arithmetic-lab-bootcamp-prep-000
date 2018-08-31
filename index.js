e4af28877a02ef127ee0f2f635f40a5f524faa96
kathinka martinsen <kathinkamartinsen@gmail.com>
2018-08-31 18:40:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,13 +28,10 @@ function dec(n){
 }
 
 function makeInt(n){
-parseInt()
-parseInt(n, 10)
-  return (n);
+  return parseInt(n, 10);
 }
 
 function preserveDecimal(n){
-  parseFloat('n', 10);
   return parseFloat(n);
 }
 
